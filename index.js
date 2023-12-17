c61f2b9344c42220720a047d365169a2e7e0b105
Shamim Ansari <codingmywork@gmail.com>
2023-12-18 02:46:39 +0530

    index file edited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('Welcome to Kode2Cloud Freelancing'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
 
