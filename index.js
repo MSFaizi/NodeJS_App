1ea95a7b717b7de7b1812260396018b900c4907a
Shamim Ansari <codingmywork@gmail.com>
2023-12-18 03:31:17 +0530

    index file edited

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; // or any other port above 1024
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Kode2Cloud! We provide DevOps and Cloud freelancing services on fiverr.');
+  res.send('Welcome to Kode2Cloud! We provide DevOps and Cloud freelancing services on upwork.');
 });
 
 app.listen(port, () => {
