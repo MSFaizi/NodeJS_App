f90c87d59f16c3de0ac2adbd21fe5a32b0e10af1
Shamim Ansari <codingmywork@gmail.com>
2023-12-19 01:39:36 +0530

    index file edited

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to Kode2Cloud! We provide freelancing services on Fiverr.'); //write a response to the client
+  res.write('Welcome to Kode2Cloud! We provide freelancing services on.'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
